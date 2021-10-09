d5089cb0d8ba96614d714a1a1157620391c50bbf
Gustavo Silva Quieregato <quieregatog@alunos.utfpr.edu.br>
2021-10-09 17:19:30 -0300

    create to module subjects

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StudentsModule } from './students/students.module';
 import { ConfigModule } from '@nestjs/config';
+import { SubjectsModule } from './subjects/subjects.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), StudentsModule],
+  imports: [ConfigModule.forRoot(), StudentsModule, SubjectsModule],
   controllers: [],
   providers: [],
 })
