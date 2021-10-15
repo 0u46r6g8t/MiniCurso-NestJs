8d42d2016c363fca427b600eed027ee35d84cd6c
Gustavo Silva Quieregato <quieregatog@alunos.utfpr.edu.br>
2021-10-15 10:48:00 -0300

    target

--- src/students/typeorm/student.entity.ts
+++ src/students/typeorm/student.entity.ts
@@ -2,7 +2,6 @@ import BasicEntity from 'src/basic.entity';
 import { Exclude } from 'class-transformer';
 import { Entity, Column, OneToMany, ManyToMany, JoinTable } from 'typeorm';
 import { Subject } from 'src/subjects/typeorm/subject.entity';
-
 @Entity('tb_students')
 export class Student extends BasicEntity {
   @Column({
